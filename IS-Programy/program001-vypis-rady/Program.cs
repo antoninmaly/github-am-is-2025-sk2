9e612054eba6e53d83da3e49b6bc00749efe8eb2
Antonín Malý <antonin.maly@tul.cz>
2025-10-02 14:07:46 +0200

    #3 Výpis řady - vstup první hodnoty

---
+++ IS-Programy/program001-vypis-rady/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Win32.SafeHandles;
+
+string again = "a";
+while (again == "a");
+{
+    Console.Clear();
+    Console.WriteLine("******** Výpis číselné řady *********");
+    Console.WriteLine("*************************************");
+    Console.WriteLine("*************************************");
+    Console.WriteLine("********** Antonín Malý *************");
+    Console.WriteLine("************ 2.10.2025 **************");
+    Console.WriteLine("*************************************");
+    Console.WriteLine("*************************************");
+    Console.WriteLine();
+
+
+    // Vstup hodnot do programu, ale špatně řešený
+    //Console.Write("Zadejte první číslo řady: ");
+    //int first = int.Parse(Console.ReadLine());
+
+
+    //Vstup hodnoty do programu, řešený lépe
+    Console.Write("Zadejte první čílso řady (celé číslo): ");
+    int first;
+    while (int.TryParse(Console.ReadLine(), out first));
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte a");
+    again = Console.ReadLine();
+
+}
