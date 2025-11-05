23444b5b4f69751bbd511fb5781a259dbc83d1e1
Antonín Malý <antonin.maly@tul.cz>
2025-11-05 11:00:29 +0100

    #7 Trojúhelník - základ projektu

---
+++ IS-Programy/program004b-pravouhly-trojuhelnik/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Win32.SafeHandles;
+
+string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("***** Vykreslení pravoúhlého troúhelníku ***");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************* Antonín Malý *****************");
+    Console.WriteLine("************** 5.11.2025 *******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+    Console.Write("Zadejte první číslo řady (celé číslo): ");
+    int first;
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
+    }
+
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+}                                                   
