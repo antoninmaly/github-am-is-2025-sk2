4bb2964a1a6ad12149e104f1175490df306b0bf1
Antonín Malý <antonin.maly@tul.cz>
2025-12-04 13:21:01 +0100

    #13 - Výpočet Pí - vypsání pí

---
+++ IS-Programy/program010a-vypocet-pi/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Win32.SafeHandles;
+
+string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("***** Konvergentní řady - výpočet Pí *******");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************* Antonín Malý *****************");
+    Console.WriteLine("************** 4.12.2025 *******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+    Console.Write("Zadejte poslední číslo řady (kladné číslo < 1): ");
+    double presnost;
+    while (!double.TryParse(Console.ReadLine(), out presnost))
+    {
+        Console.Write("Nezadali jste kladné číslo < 1. Zadejte posední číslo řady znovu: ");
+    }
+
+    double i = 1;
+    double piCtvrt = 1;
+    double znamenko = 1;
+    double pi;
+
+    while (1/i>=presnost)
+    {
+        i = i+2;
+        znamenko = - znamenko;
+        piCtvrt = piCtvrt + znamenko *1/i;
+    }
+
+   
+    pi = 4*piCtvrt;
+    Console.WriteLine();
+    Console.WriteLine($"Hodnota Pí je: {pi}");
+
+
+
+
+
+
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+}                                                   
