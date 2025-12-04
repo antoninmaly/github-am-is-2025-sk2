26cd1bab1369f19f094c89d745573cc56bc6060f
Antonín Malý <antonin.maly@tul.cz>
2025-12-04 13:59:15 +0100

    #12 Převod z 10do2 soustavy - nevim

--- IS-Programy/program009a-10do2/Program.cs
+++ IS-Programy/program009a-10do2/Program.cs
@@ -21,6 +21,31 @@ while (again == "a")
         Console.Write("Nezadali jste přirozené číslo. Zadejte přirozené číslo znovu: ");
     }
 
+    uint[] myArray = new uint[32]; //32 je počet buňek pro maximální hodnotu
+    uint backupNumber10 = number10;
+    uint zbytek;
+
+    uint i;
+    for(i = 0; number10 > 0 ; i++)
+    {
+        zbytek = number10 % 2;
+        number10 = (number10 - zbytek)/2;
+        myArray[i] = zbytek;
+
+        Console.WriteLine($"Celá část: {number10}; Zbytek: {zbytek} ");
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("=============================");
+    Console.WriteLine($"Desítkové číslo {backupNumber10} ve dvojkové soustavě je ");
+    for(uint j = i - 1; j>=0 ;j--)
+    {
+        Console.Write($"{myArray[j]}");
+    }
+
+
+
+
 
 
     Console.WriteLine();
