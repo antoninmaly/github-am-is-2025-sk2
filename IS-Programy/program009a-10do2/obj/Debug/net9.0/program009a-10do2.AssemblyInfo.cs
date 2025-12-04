7419cbc43258caaef40b81e78476a72068352f18
Antonín Malý <antonin.maly@tul.cz>
2025-12-04 13:52:55 +0100

    #12 Převod z 10do2 - zbytek programu

--- IS-Programy/program009a-10do2/obj/Debug/net9.0/program009a-10do2.AssemblyInfo.cs
+++ IS-Programy/program009a-10do2/obj/Debug/net9.0/program009a-10do2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("program009a-10do2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8c3d213c554572953b3b2cd885580ed0372cafb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31e39cebcea4b23277e605065547b06bc8b0d9b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("program009a-10do2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("program009a-10do2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
