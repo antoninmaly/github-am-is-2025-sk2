fa776b0bd16a5f867f6f0b66fbc884e06513eb0d
Antonín Malý <antonin.maly@tul.cz>
2025-12-20 15:53:46 +0100

    #4 Základní kód

--- IS-Programy/program000a-zakladni-kod/Program.cs
+++ IS-Programy/program000a-zakladni-kod/Program.cs
@@ -1,15 +1,15 @@
-﻿﻿using Microsoft.Win32.SafeHandles;
+using Microsoft.Win32.SafeHandles;
 
 string again = "a";
 while (again == "a")
 {
     Console.Clear();
     Console.WriteLine("********************************************");
-    Console.WriteLine("*********** název programu *************");
+    Console.WriteLine("********>*** název programu **********>>>***");
     Console.WriteLine("********************************************");
     Console.WriteLine("********************************************");
     Console.WriteLine("************* Antonín Malý *****************");
-    Console.WriteLine("************** 1.1.2025 *******************");
+    Console.WriteLine("************** 1.1.2025 *****************>**");
     Console.WriteLine("********************************************");
     Console.WriteLine("********************************************");
     Console.WriteLine();
