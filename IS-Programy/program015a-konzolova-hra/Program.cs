73dd4a847cb59b51c476bb4d70ef5cfc957c059b
Antonín Malý <antonin.maly@tul.cz>
2025-12-23 15:26:26 +0100

    #17 Hra - výchozí projekt

---
+++ IS-Programy/program015a-konzolova-hra/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Win32.SafeHandles;
+
+string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("*********************************");
+    Console.WriteLine("*********** PENALTY *************");
+    Console.WriteLine("*********************************");
+    Console.WriteLine("********* Antonín Malý **********");
+    Console.WriteLine("*********** 1.1.2025 ************");
+    Console.WriteLine("*********************************");
+    Console.WriteLine();
+
+    
+
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pokud si chcete zahrát znovu - stiskněte klávesu a");
+    again = Console.ReadLine();
+
+}                                                   
